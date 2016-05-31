69bde5062a5fc7cfdd72d84786591c5a5708fbdb
Andrew Caldwell <andrew512@gmail.com>
2016-05-31 10:59:16 -0500

    Fixing syntax issue in wallaby.js

--- skeleton-typescript/wallaby.js
+++ skeleton-typescript/wallaby.js
@@ -1,53 +1,51 @@
 module.exports = function (wallaby) {
 
     return {
-            files: [
+        files: [
 
-                {pattern: 'jspm_packages/system.js', instrument: false},
-                {pattern: 'config.js', instrument: false},
+            {pattern: 'jspm_packages/system.js', instrument: false},
+            {pattern: 'config.js', instrument: false},
 
-                {pattern: 'src/**/*.ts', load: false},
-                {pattern: 'test/unit/setup.ts', load: false}
-            ],
+            {pattern: 'src/**/*.ts', load: false},
+            {pattern: 'test/unit/setup.ts', load: false}
+        ],
 
-            tests: [
-                {pattern: 'test/unit/**/*.spec.ts', load: false}
-            ],
+        tests: [
+            {pattern: 'test/unit/**/*.spec.ts', load: false}
+        ],
 
 
-            middleware: (app, express) = > {
+        middleware: function (app, express) {
             app.use('/jspm_packages', express.static(require('path').join(__dirname, 'jspm_packages')));
-},
+        },
 
-    bootstrap: function (wallaby) {
-        wallaby.delayStart();
+        bootstrap: function (wallaby) {
+            wallaby.delayStart();
 
-        System.config({
-            paths: {
-                "*": "*"
+            System.config({
+                paths: {
+                    "*": "*"
+                }
+            });
+
+            var promises = [];
+            for (var i = 0, len = wallaby.tests.length; i < len; i++) {
+                promises.push(System['import'](wallaby.tests[i].replace(/\.js$/, '')));
             }
-        });
 
-        var promises = [];
-        for (var i = 0, len = wallaby.tests.length; i < len; i++) {
-            promises.push(System['import'](wallaby.tests[i].replace(/\.js$/, '')));
-        }
+            System.import('test/unit/setup')
+                .then(function () {
+                    return Promise.all(promises);
+                })
+                .then(function () {
+                    wallaby.start();
+                }).catch(function (e) {
+                setTimeout(function () {
+                    throw e;
+                }, 0);
+            });
+        },
 
-        System.import('test/unit/setup')
-            .then(function () {
-                return Promise.all(promises);
-            })
-            .then(function () {
-                wallaby.start();
-            }).catch(function (e) {
-            setTimeout(function () {
-                throw e;
-            }, 0);
-        });
-    }
-    ,
-
-    debug: false
-}
-    ;
+        debug: false
+    };
 };
